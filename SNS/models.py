29bd6a9876e0195303e2b28eecb7e93424903cb0
Soma Ando <soma.ando.1227@gmail.com>
2024-03-02 16:51:34 +0900

    Add models profileData

--- SNS/models.py
+++ SNS/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class profileData(models.Model):
+    user_id = models.CharField(max_length=50, unique=True)
+    icon = models.URLField()
+    description = models.TextField(max_length=300)
+    spotify_id = models.CharField(max_length=100)
