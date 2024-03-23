dd05c1abe398d06a412a508fddd5e2f18c30cfea
Soma Ando <soma.ando.1227@gmail.com>
2024-03-23 19:06:14 +0900

    Add admin page

--- SNS/admin.py
+++ SNS/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 
-from .models import ProfileData
+from .models import *
+
 admin.site.register(ProfileData)
+admin.site.register(MusicFollowData)
+admin.site.register(UserFollowData)
+admin.site.register(PostData)
+admin.site.register(GoodData)
