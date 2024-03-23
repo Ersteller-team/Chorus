58048873cbb4cb60b911ed95252a318bd4586a57
Soma Ando <soma.ando.1227@gmail.com>
2024-03-23 18:43:26 +0900

    Fixed no icon bugs

--- SNS/context_processors.py
+++ SNS/context_processors.py
@@ -1,8 +1,11 @@
 from .models import ProfileData
+from .constants import *
 
 def icon(request):
     if request.user.is_authenticated:
         user_profile = ProfileData.objects.filter(user_id=request.user.id)
+        if user_profile[0].icon == None:
+            user_profile[0].icon = DEFAULT_ARTIST_IMAGE
         return { 'icon': user_profile[0].icon }
     else:
         user_profile = None
